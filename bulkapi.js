39a36c1ad03cf4ee675eecf5085c3043a6379f94
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-05-23 12:57:55 +1000

    Add files via upload

--- bulkapi.js
+++ bulkapi.js
@@ -1,5 +1,12 @@
 function fn_bulkapi_execute(crytos) {
   console.log("bulk api function has been called.  Adding Bulk Queries");
+
+  // Require taapi: npm i taapi --save
+  const taapi = require("taapi");
+
+  // Setup client with authentication
+  const client = taapi.client("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvbmdhYm91dHNpYmJpZUBnbWFpbC5jb20iLCJpYXQiOjE2NTA2Nzg1MjEsImV4cCI6Nzk1Nzg3ODUyMX0.kB2EUss32pvD6D3Nv6pg92-ziJ_phjX722Qqx_eHbtU");
+
   client.initBulkQueries();
 
 	for (let i=0; i < cryptos.length; i++) {
--- pattern-matching.js
+++ pattern-matching.js
@@ -1,12 +1,7 @@
 // pass is desired crypto as an input and run through a set of indicators
 
-// Require taapi: npm i taapi --save
-const taapi = require("taapi");
 
-// Setup client with authentication
-const client = taapi.client("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvbmdhYm91dHNpYmJpZUBnbWFpbC5jb20iLCJpYXQiOjE2NTA2Nzg1MjEsImV4cCI6Nzk1Nzg3ODUyMX0.kB2EUss32pvD6D3Nv6pg92-ziJ_phjX722Qqx_eHbtU");
-
-// set crypto to run analysis on based on command line arguments
+// set crypto and time period to run analysis on based on command line arguments
 const crypto_lookup = require("./crypto-lookup.js");
 const myArgs = process.argv.slice(2);
 crypto = crypto_lookup.fn_crypto_lookup(myArgs[0]);
