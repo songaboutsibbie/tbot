72a3ae375c181f49448ae7e1b6afdeb0d5fce0ad
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-05-23 13:44:33 +1000

    Add files via upload

--- bulkapi.js
+++ bulkapi.js
@@ -14,9 +14,9 @@ function fn_bulkapi_execute(pattern, crypto, time_period) {
  		client.addBulkQuery(pattern[i], "binance", crypto, time_period);	
 	}
 
-	console.log("Execute bulk api");
+	console.log("Execute bulk api for first time");
  	client.executeBulkQueries().then(result => {
-    console.log("results are as follows");
+    console.log("results for first iteration are as follows");
     console.log(result);
    	for (let i=0; i < result.length; i++) {
     if (result[i].result.value > 80) {
@@ -31,23 +31,23 @@ function fn_bulkapi_execute(pattern, crypto, time_period) {
 
   client.initBulkQueries();
   setTimeout(function() {
-  for (let i=0; i < pattern.length; i++) {
-    console.log ("adding bulkquery with" + pattern[i] + crypto + time_period ) ;
-    client.addBulkQuery(pattern[i], "binance", crypto, time_period);  
-  }
-
-  console.log("Execute bulk api");
-  client.executeBulkQueries().then(result => {
-    console.log("results are as follows");
-    console.log(result);
-    for (let i=0; i < result.length; i++) {
-    if (result[i].result.value > 80) {
-      console.log(result[i].indicator + "pattern matches with value of " + result[i].result.value);
+    for (let i=0; i < pattern.length; i++) {
+      console.log ("adding bulkquery with" + pattern[i] + crypto + time_period ) ;
+      client.addBulkQuery(pattern[i], "binance", crypto, time_period);  
     }
-  }
-  }).catch(error => {
-      console.log(error);
-  });
+
+    console.log("Execute bulk api for second time");
+    client.executeBulkQueries().then(result => {
+      console.log("results for second iteration are as follows");
+      console.log(result);
+      for (let i=0; i < result.length; i++) {
+      if (result[i].result.value > 80) {
+        console.log(result[i].indicator + "pattern matches with value of " + result[i].result.value);
+      }
+    }
+    }).catch(error => {
+        console.log(error);
+    });
   }, 15000) ;
 
 }
