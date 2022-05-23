96c13edb9eda3015d8cd8859c1b1a71ffe8c8dde
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-05-23 13:02:15 +1000

    Add files via upload

--- pattern-matching.js
+++ pattern-matching.js
@@ -13,7 +13,7 @@ console.log('time period to run analysis on is: ', time_period);
 // call bulkapi execute function in 3 increments 15 seconds apart
 const bulkapi = require("./bulkapi.js");
 
-result = bulkapi.fn_bulkapi_execute("crows", "backcrows");
+result = bulkapi.fn_bulkapi_execute(["crows", "backcrows"], crypto, time_period);
 
 console.log("pattern results for first exeuction");
 console.log(result);
