b5a84abfb37acb5f5992c13ce64829782616a6e4
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-05-23 12:03:56 +1000

    removed redundant function import

--- pattern-matching.js
+++ pattern-matching.js
@@ -6,11 +6,8 @@ const taapi = require("taapi");
 // Setup client with authentication
 const client = taapi.client("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvbmdhYm91dHNpYmJpZUBnbWFpbC5jb20iLCJpYXQiOjE2NTA2Nzg1MjEsImV4cCI6Nzk1Nzg3ODUyMX0.kB2EUss32pvD6D3Nv6pg92-ziJ_phjX722Qqx_eHbtU");
 
-// import crypto list & analysis modules
-const mom_indicators = require("./momentum-indicators.js");
-const crypto_lookup = require("./crypto-lookup.js");
-
 // set crypto to run analysis on based on command line arguments
+const crypto_lookup = require("./crypto-lookup.js");
 const myArgs = process.argv.slice(2);
 crypto = crypto_lookup.fn_crypto_lookup(myArgs[0]);
 console.log('crypto to run analysis on is: ', crypto);
