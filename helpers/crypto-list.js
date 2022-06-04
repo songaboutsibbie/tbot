c1e642480b9be968b5998c373b930e73915df295
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-06-04 16:11:12 +1000

    Add files via upload

--- helpers/crypto-list.js
+++ helpers/crypto-list.js
@@ -1,7 +1,7 @@
 function fn_crypto_list(priority) {
 
 
-const crypto_list_one = ["BTC/USDT", "ETH/USDT", "AVAX/USDT", "MANA/USDT", "XRP/USDT", "ADA/USDT", "SOL/USDT", "DOT/USDT", "SHIB/USDT", "NEAR/USDT", "ALGO/USDT", "UNI/USDT", "AXS/USDT", "GMT/USDT", "KSM/USDT", "ENJ/USDT", "BAT/USDT", "STXU/USDT", "KAVA/USDT", "MINA/USDT"];
+const crypto_list_one = ["BTC/USDT", "ETH/USDT", "AVAX/USDT", "MANA/USDT", "XRP/USDT", "ADA/USDT", "SOL/USDT", "DOT/USDT", "SHIB/USDT", "NEAR/USDT", "ALGO/USDT", "UNI/USDT", "AXS/USDT", "GMT/USDT", "KSM/USDT", "ENJ/USDT", "BAT/USDT", "STX/USDT", "KAVA/USDT", "MINA/USDT"];
 const crypto_list_two = ["KNC/USDT", "POWR/USDT", "TORN/USDT", "VIDT/USDT", "BNX/USDT" ]
 
 if (priority == 1 ) { return crypto_list_one ; }
