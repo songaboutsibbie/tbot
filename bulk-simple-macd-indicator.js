99ca52b8f1119b8f4bcd8b3054b351aa8953d8d1
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-06-04 19:45:50 +1000

    Add files via upload

--- bulk-simple-macd-indicator.js
+++ bulk-simple-macd-indicator.js
@@ -7,16 +7,21 @@ const taapi = require("taapi");
 const client = taapi.client("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvbmdhYm91dHNpYmJpZUBnbWFpbC5jb20iLCJpYXQiOjE2NTA2Nzg1MjEsImV4cCI6Nzk1Nzg3ODUyMX0.kB2EUss32pvD6D3Nv6pg92-ziJ_phjX722Qqx_eHbtU");
 const crypto_list_helper = require("./helpers/crypto-list.js");
 
+// setup slack notifications
+const slack = require("./helpers/slack_notification.js");
+
+// Pull in crypto list 1
 crypto_list = crypto_list_helper.fn_crypto_list(1);
 
 
+// Load up 20 queries to execute in bulk
 client.initBulkQueries();
-
 for (let i=0; i < crypto_list.length; i++) {
   client.addBulkQuery("macd", "binance", crypto_list[i], "4h");
 
 }
 
+//execute queries and display buy or sell results
 client.executeBulkQueries().then(result => {
   console.log(result);
 
@@ -32,6 +37,8 @@ client.executeBulkQueries().then(result => {
       //curl -X POST -H 'Content-type: application/json' --data '{"text":"test"}' https://hooks.slack.com/services/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
     }
   }
+
+  slack.fn_sendmessage("hello world");
 
 }).then( result => {
   setTimeout(function() {
