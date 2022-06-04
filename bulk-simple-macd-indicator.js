fd80543688092708f811d28a8d2c205b017ac28d
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-06-04 16:13:04 +1000

    Add files via upload

--- bulk-simple-macd-indicator.js
+++ bulk-simple-macd-indicator.js
@@ -23,7 +23,7 @@ client.executeBulkQueries().then(result => {
   console.log(result);
 
   for (let i=0; i < result.length; i++) {
-    console.log(result[i].valueMACDHist);
+    console.log(result[i].result.valueMACDHist);
   }
 
 }).catch(error => {
