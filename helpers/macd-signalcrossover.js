23801399d6b9f55b27a1569ba1a2f3ab205950fb
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-06-05 19:23:18 +1000

    Add files via upload

--- helpers/macd-signalcrossover.js
+++ helpers/macd-signalcrossover.js
@@ -3,9 +3,9 @@ function fn_recency(response) {
   for (let i=0; i < response.data.length; i++) { 
     if (response.data[i].valueMACD > response.data[i].valueMACDSignal) { 
       recency = i+1;
-      console.log("cross over happened " + recency + " time periods ago");
-      return recency;
     }
+    console.log("cross over happened " + recency + " time periods ago");
+    return recency;
   }
 
 }
