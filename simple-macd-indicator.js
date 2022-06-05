039d146044dfa4343c240ebc5ae8a4378c789c71
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-06-05 19:49:48 +1000

    Add files via upload

--- simple-macd-indicator.js
+++ simple-macd-indicator.js
@@ -1,18 +1,22 @@
 // provide a buy recommendation using macd algo
 
-// set crypto and time period to run analysis on based on command line arguments
-
+// set crypto (from args) & hard code time period and backtrack perios
 const myArgs = process.argv.slice(2);
 crypto = myArgs[0];
 time_period = "4h"
 bt_length = 12; // 3 days using 4h time period
 
-console.log("running analysis on " + crypto + "going back " + bt_length + " time periods of " + time_period);
+// setup slack notifications
+const slack = require("./helpers/slack-notification.js");
 
+// import macd helper functions
 const macd_sigcros = require("./helpers/macd-signalcrossover.js");
 
-var axios = require('axios');
 
+console.log("running analysis on " + crypto + "going back " + bt_length + " time periods of " + time_period);
+
+// retrieve macd indicators from taapi
+var axios = require('axios');
 axios.get('https://api.taapi.io/macd', {
   params: {
     secret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvbmdhYm91dHNpYmJpZUBnbWFpbC5jb20iLCJpYXQiOjE2NTA2Nzg1MjEsImV4cCI6Nzk1Nzg3ODUyMX0.kB2EUss32pvD6D3Nv6pg92-ziJ_phjX722Qqx_eHbtU",
@@ -25,16 +29,12 @@ axios.get('https://api.taapi.io/macd', {
 .then(function (response) {
   
   // starting conditions are a) MACD below signal line at start of time period & MACD has risen above signal in most recent time period
-  console.log(response.data);
-  console.log(response.data[11].valueMACD);
-  console.log(response.data[11].valueMACDSignal);
-  console.log(response.data[0].valueMACDHist);
   
   if (response.data[11].valueMACD < response.data[11].valueMACDSignal && response.data[0].valueMACDHist > 0) {
     console.log("\n ** detected signal cross over.  checking when it happened");
 
     // check recency and only
-    macd_sigcros.fn_recency(response);
+    recency = macd_sigcros.fn_recency(response);
 
     // check sharpness of upturn
     // fn_macd_rateofchange
@@ -43,8 +43,14 @@ axios.get('https://api.taapi.io/macd', {
     // fn_macd_zerolineposition
 
     // send slack message only if recency and sharpness are of a high enough rating
-    // code here
+    if(recency < 6 ) {
+      msg = "Buy " + crypto + "  :  MACD signal crossover occured" + recency + " time periods ago";
+      slack.fn_sendmessage(msg);
+    }
   }
+
+
+
 
 })
 .catch(function (error) {
