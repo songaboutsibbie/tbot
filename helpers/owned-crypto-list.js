3223491e1d528abd260191a0822e2954367e7095
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-06-06 16:04:37 +1000

    Add files via upload

---
+++ helpers/owned-crypto-list.js
@@ -0,0 +1,11 @@
+function fn_get_crypto_list(priority) {
+
+// 0 = owned , 1 = highest priority, 2 = second priority
+
+const crypto_list_owned = ["ETH/USDT", "SOL/USDT"]
+return crypto_list_owned;
+
+}
+
+module.exports = { fn_get_crypto_list } ;
+
