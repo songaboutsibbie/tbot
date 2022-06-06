4139843c782f087b3508008d5f96cf99e2d893fd
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-06-06 13:00:45 +1000

    Add files via upload

--- helpers/macd-signalcrossover.js
+++ helpers/macd-signalcrossover.js
@@ -12,21 +12,25 @@ function fn_recency(response) {
 
 function fn_rateOfIncrease(response) {
   var lowPoint = response.data[0].valueMACD;
+
+  // find the point at which MACD was at its lowest value.  
   for (let i=0; i < response.data.length; i++) { 
     if (response.data[i].valueMACD < lowPoint) { lowPoint = response.data[i].valueMACD; lowPointPosition = i+1; }  
   }
   
-  console.log("Low Point is : " + lowPoint);
-  console.log("Low Point Position is : " + lowPointPosition);
-
+  // Calculate the total increase between lowest MACD value and most recent MACD Value
   totalIncrease = response.data[0].valueMACD - lowPoint;
-  console.log("Total Increase " + totalIncrease);
 
+  // Calculate the average increase
   averageIncrease = totalIncrease / lowPointPosition;
-  console.log("Average Increase " + averageIncrease);
 
+  // Calculate ther rate of increase as a percentage
   rateOfIncrease = (averageIncrease / totalIncrease ) * 100;
 
+  //console.log("Low Point is : " + lowPoint);
+  //console.log("Low Point Position is : " + lowPointPosition);
+  //console.log("Total Increase " + totalIncrease);
+  // console.log("Average Increase " + averageIncrease);
   return rateOfIncrease;
 }
 
