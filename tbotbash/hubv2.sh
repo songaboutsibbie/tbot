ea93dc077dec63dd207fb4ee807b4dd748775c69
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-06-06 15:44:27 +1000

    Add files via upload

--- tbotbash/hubv2.sh
+++ tbotbash/hubv2.sh
@@ -1,10 +1,14 @@
-#!/usr/bin/env bash
- 
+#!/bin/bash
+
 cd /home/homestar/tbot
-readarray -t arrCrypto < ~/tbot/data/cryptolist.txt
-echo ${arrCrypto[0]}
-echo ${arrCrypto[1]}
-echo ${arrCrypto[2]}
+IFS=$'\n' read -d '' -r -a arrCrypto < ~/tbot/data/buylist.txt
 
-
-node simple-macd-indicator.js ${arrCrypto[0]}
+for i in "${arrCrypto[@]}"
+do
+        :
+        crypto="$i"
+        crypto=`echo $i | sed 's/ *$//g'`
+        echo " crypto being passed in is $crypto"
+        node simple-macd-indicator.js "$crypto"
+        sleep 15
+done
