29685b1d955a6ef0a490849bccc20dc86c9a310c
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-12-03 12:04:20 +1100

    Add files via upload

--- macd-3pass.js
+++ macd-3pass.js
@@ -38,13 +38,17 @@ client.executeBulkQueries().then(result => {
   console.log(result[0].result.valueMACD);
 
   // set start and finish positions of each macd interval type
-  startPos15m = 0; endPos15m = 2;
-  startPos4h = 3; endPos4h = 5;
-  startPos1d = 6; endPos1d = 8;
+  startPos15m = 0; endPos15m = 2; startPos4h = 3; endPos4h = 5; startPos1d = 6; endPos1d = 8;
 
 
-// 1st check : did macd cross the signal line in the withing the last 4 15min interval data points
-    if ( macd_helper.fn_checkCrossover(result, startPos15m, endPos15m) == true ) { indicator_score = 100} else { indicator_score = -1000; }
+  // 1st wave : check if macd cross the signal line in the withing the last 4 15min interval data points
+  if ( macd_helper.fn_checkCrossover(result, startPos15m, endPos15m) == true ) { indicator_score = 100} else { indicator_score = -1000; }
+
+  // 2nd wave  : if trending up +50 ; if below signal cross over + trending up +100 ; if neither reste to -1000
+  if ( macd_helper.fn_checkTrend(result, startPos15m, endPos15m) == true && result[endPos4h].result.valueMACDHist < 0) 
+    { indicator_score = indicator_score + 100 ; console.log("4h got 100 points")}
+  else { indicator_score = -1000 ;}
+
    
 
   console.log("indicator score : " + indicator_score);
