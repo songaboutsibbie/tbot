5abe7f2bb1e2e262031fee2766abc40bd624d9b9
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-12-05 17:41:01 +1100

    Add files via upload

--- helpers/macd-helper.js
+++ helpers/macd-helper.js
@@ -1,6 +1,6 @@
 function fn_checkCrossover(result, startPos, endPos) {
   //check whether the MACDHist was negative at start of results and positrive and end of reuslts indicating a crossover has occured
-  console.log("checking crossover : is " + result[endPos].result.valueMACDHist + " < 0  && " + result[startPos].result.valueMACDHist + " > 0");  // debug
+  //console.log("checking crossover : is " + result[endPos].result.valueMACDHist + " < 0  && " + result[startPos].result.valueMACDHist + " > 0");  // debug log
 
   if (result[endPos].result.valueMACDHist < 0 && result[startPos].result.valueMACDHist > 0) { console.log("crossover true") ; return true; }
   else { console.log("Crossover set to : false") ; return false ; }
@@ -9,16 +9,16 @@ function fn_checkCrossover(result, startPos, endPos) {
 
 // this function confirms that the value of MACD increases over time (array is sorted in reverse chronological order)
 function fn_checkTrend(result, startPos, endPos) {  
-  console.log("checking trend with endPos of " + endPos + " and startPos of " + startPos);  // debug comments
+  //console.log("checking trend with endPos of " + endPos + " and startPos of " + startPos);  // debug log
   trend = false;
 
   for (let i=endPos; i > startPos; i--) {
-    console.log("checking whether " + result[i].result.valueMACD + " < " + result[i-1].result.valueMACD + "?");
+    //console.log("checking whether " + result[i].result.valueMACD + " < " + result[i-1].result.valueMACD); // debug log
     if (Number(result[i].result.valueMACD) < Number(result[i-1].result.valueMACD)) {  trend = true ; } 
     else { trend = false ; i = i-100; }
   }
 
-  console.log("Trend set to : " + trend);
+  //console.log("Trend set to : " + trend); // debug log
   return trend;
 }
 
