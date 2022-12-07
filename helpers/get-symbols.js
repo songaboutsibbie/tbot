3170f9797c30023c32dc8eba3be2b3dcb46d6470
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-12-07 17:09:47 +1100

    Add files via upload

--- helpers/get-symbols.js
+++ helpers/get-symbols.js
@@ -1,20 +1,22 @@
-// Retrieves full list of symbols from binance exchange and then filters for only USDT pairs
+//    retrieves a list of symbols on the kucoin exchange and filters for only USDT pairs
 
-// Require axios: npm i axios
-var axios = require('axios');
+const axios = require('axios')
 
-axios.get('https://api.taapi.io/exchange-symbols', {
-  params: {
-    secret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvbmdhYm91dHNpYmJpZUBnbWFpbC5jb20iLCJpYXQiOjE2NTA2Nzg1MjEsImV4cCI6Nzk1Nzg3ODUyMX0.kB2EUss32pvD6D3Nv6pg92-ziJ_phjX722Qqx_eHbtU",
-    exchange: "binance"
-  }
-})
-.then(function (response) {
-  
-  let symbolListUSDT = response.data.filter(a => new RegExp('\/USDT$').test(a));
- 
-  console.log(symbolListUSDT);
-})
-.catch(function (error) {
-  console.log(error.response.data);
-});
+const getSymbol = async () => {
+    try {
+        const resp = await axios.get('https://api.taapi.io/exchange-symbols', {
+          headers: { 'Accept-Encoding': 'application/json'},
+          params: {
+            secret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjI2MzVhZjk0MjI0NmNlM2IwNGMzNDJkIiwiaWF0IjoxNjcwMjI5MzYwLCJleHAiOjMzMTc0NjkzMzYwfQ.QVROcNEXCK41L_4-9kcq3RB06s3L0mfFq4ir2MdcTlY",
+            exchange: "kucoin",
+          }
+        });
+        let symbolListUSDT = resp.data.filter(a => new RegExp('\/USDT$').test(a));
+        console.log(JSON.stringify(symbolListUSDT, null, 4));
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+    }
+};
+
+getSymbol();
