f6f9b0ee1aa35e7b0700c08e13c0ef9d01810197
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-12-07 16:57:30 +1100

    Add files via upload

--- sandpit.js
+++ sandpit.js
@@ -2,27 +2,13 @@
 
 const axios = require('axios');
 const sleep = require("./helpers/sleep.js");
+const getSymbol = require("./helpers/fnget-symbols.js");
 
 
-const getSymbol = async () => {
-    try {
-        const resp = await axios.get('https://api.taapi.io/exchange-symbols', {
-          headers: { 'Accept-Encoding': 'application/json'},
-          params: {
-            secret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjI2MzVhZjk0MjI0NmNlM2IwNGMzNDJkIiwiaWF0IjoxNjcwMjI5MzYwLCJleHAiOjMzMTc0NjkzMzYwfQ.QVROcNEXCK41L_4-9kcq3RB06s3L0mfFq4ir2MdcTlY",
-            exchange: "kucoin",
-          }
-        });
-        let symbolListUSDT = resp.data.filter(a => new RegExp('\/USDT$').test(a));
-        console.log(JSON.stringify(symbolListUSDT, null, 4));
-        await sleep.fn_sleep(3000);
-        console.log("sleep over");
-    } catch (err) {
-        // Handle Error Here
-        console.error(err);
-    }
-};
+symbolList = getSymbol.fn_getSymbol();
+console.log(JSON.stringify(symbolList, null, 4));
+await sleep.fn_sleep(3000);
+console.log("sleep over");
 
 
-getSymbol();
 
