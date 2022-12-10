b1e1a295d7ecf95ad9d4fea465adad19023acf92
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-12-11 09:09:35 +1100

    Add files via upload

---
+++ helpers/check-macd.js
@@ -0,0 +1,26 @@
+function fn_checkCrossover(result, startPos, endPos) {
+  //check whether the MACDHist was negative at start of results and positrive and end of reuslts indicating a crossover has occured
+  //console.log("checking crossover : is " + result[endPos].result.valueMACDHist + " < 0  && " + result[startPos].result.valueMACDHist + " > 0");  // debug log
+
+  if (result[endPos].result.valueMACDHist < 0 && result[startPos].result.valueMACDHist > 0) { return true; }
+  else { return false ; }
+   
+}
+
+// this function confirms that the value of MACD increases over time (array is sorted in reverse chronological order)
+function fn_checkTrend(result, startPos, endPos) {  
+  //console.log("checking trend with endPos of " + endPos + " and startPos of " + startPos);  // debug log
+  trend = false;
+
+  for (let i=endPos; i > startPos; i--) {
+    //console.log("checking whether " + result[i].result.valueMACD + " < " + result[i-1].result.valueMACD); // debug log
+    if (Number(result[i].result.valueMACD) < Number(result[i-1].result.valueMACD)) {  trend = true ; } 
+    else { trend = false ; i = i-100; }
+  }
+
+  //console.log("Trend set to : " + trend); // debug log
+  return trend;
+}
+
+
+module.exports = { fn_checkCrossover, fn_checkTrend } ;
