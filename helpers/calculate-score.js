5e237623899f45eb25a8fe2a88ed10dba61335a7
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-12-11 18:34:59 +1100

    Add files via upload

--- helpers/calculate-score.js
+++ helpers/calculate-score.js
@@ -7,8 +7,8 @@ function fn_macdShortInterval(crossover, increaseRate) {
   score = 0;
 
   if (crossover == true) {
-    score = 100;
-    if(increaseRate > 10) { score = score + 20; console.log("+20 points for rate of increase")}  
+    score = 100; console.log("+100 cross over occured recently - short interval")
+    if(increaseRate > 10) { score = score + 20; console.log("+20 points : rate of increase - short interval")}  
   }
   else { indicator_score = -1000 ; console.log("-- failed to score on short term inteval. exiting"); }
    
@@ -23,19 +23,25 @@ function fn_macdMediumInterval() {
   return score;
 }
 
+// calculates a score based on macd over 1d interval
+// if trending up score 50+ points else score -1000 and exit program
+// if crossover hasn't occured or occurred recently give a bonus 25 points
+// if rate of increase above 10% then add 20 to the score
 function fn_macdLongInterval(trend, crossover, startingMACDHistValue) {
   score = 0;
 
-  if ( trend == true && Number(startingMACDHistValue) < 0) { 
-    score = score + 100 ;  console.log("+100 points - trend up and signal line below macd"); // debug log
-  } 
-  else if ( trend == true && crossover == true) { 
-    score = indicator_score + 50;  console.log("+50 points - second wave trend up and signal line crossed over recently"); // debug log
+  if ( trend == true ) {
+    score = 50; console.log("50 points : trending up - long interval"); // debug log
+
+    if ( crossover == true ) { 
+      score = score + 25 ;  console.log("+20 points : crossed over occured recently - long term interval"); // debug log
+    }
+    if (Number(startingMACDHistValue) < 0) {
+      score = score + 25 ;  console.log("+30 points : macd hasn't crossed over yet - long term interval"); // debug log
+    }
+
   }
-  else if ( trend == true ) {
-    score = indicator_score + 10; console.log("+20 points - second wave just trending up"); // debug log
-  }
-  else { score = -1000 ; console.log("-- failed to score on Ling Term Interval.  Exiting")} 
+  else  { score = -1000 ; console.log("-- failed to score on Long Term Interval.  Exiting")} 
 
 
   return score;
