a887d05ce1d262b0436d43706232802caac42d32
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-12-12 20:57:03 +1100

    Add files via upload

--- helpers/get-symbols.js
+++ helpers/get-symbols.js
@@ -4,21 +4,41 @@
 
 const fs = require('fs');
 const axios = require('axios')
+const sort = require("./sort-array.js");
 
 const getSymbol = async () => {
     try {
-        const resp = await axios.get('https://api.taapi.io/exchange-symbols', {
+        const kucoin_response = await axios.get('https://api.taapi.io/exchange-symbols', {
           headers: { 'Accept-Encoding': 'application/json'},
           params: {
             secret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjI2MzVhZjk0MjI0NmNlM2IwNGMzNDJkIiwiaWF0IjoxNjcwMjI5MzYwLCJleHAiOjMzMTc0NjkzMzYwfQ.QVROcNEXCK41L_4-9kcq3RB06s3L0mfFq4ir2MdcTlY",
             exchange: "kucoin",
           }
         });
-        let symbolListUSDT = resp.data.filter(a => new RegExp('\/USDT$').test(a));
-        console.log(symbolListUSDT);
-        console.log(JSON.stringify(symbolListUSDT));
-        const jsonString = JSON.stringify(symbolListUSDT);
-        fs.writeFileSync('kucoin-symbols.json', jsonString);
+
+        const binance_response = await axios.get('https://api.taapi.io/exchange-symbols', {
+          headers: { 'Accept-Encoding': 'application/json'},
+          params: {
+            secret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjI2MzVhZjk0MjI0NmNlM2IwNGMzNDJkIiwiaWF0IjoxNjcwMjI5MzYwLCJleHAiOjMzMTc0NjkzMzYwfQ.QVROcNEXCK41L_4-9kcq3RB06s3L0mfFq4ir2MdcTlY",
+            exchange: "binance",
+          }
+        });
+
+        let kucoin_symbolListUSDT = kucoin_response.data.filter(a => new RegExp('\/USDT$').test(a));
+        let binance_symbolList2USDT = binance_response.data.filter(a => new RegExp('\/USDT$').test(a));
+        let merged_symbolList = sort.fn_sortArray(kucoin_symbolListUSDT, binance_symbolList2USDT);
+
+        console.log("**************** kucoin_symbolListUSDT");
+        console.log(kucoin_symbolListUSDT);
+
+        console.log("\n**************** binance_symbolList2USDT");
+        console.log(binance_symbolList2USDT);
+
+        console.log("\n**************** sorted_symbolList");
+        console.log(merged_symbolList);
+
+        const symbol_jsonString = JSON.stringify(merged_symbolList, null, 1);
+        fs.writeFileSync('./data/symbols.json', symbol_jsonString);
     } catch (err) {
         // Handle Error Here
         console.error(err);
---
+++ helpers/sort-array.js
@@ -0,0 +1,18 @@
+// an asset price is convered overbought (due for a correction) when RSI is above 70 and oversold (due for a rebound) when it is below 30
+
+function fn_sortArray(arr1, arr2) {
+
+  var arr = [];  // Array to contain match elements
+  for(var i=0 ; i<arr1.length ; ++i) {
+    for(var j=0 ; j<arr2.length ; ++j) {
+      if(arr1[i] == arr2[j]) {    // If element is in both the arrays
+        arr.push(arr1[i]);        // Push to arr array
+      }
+    }
+  }
+   
+  console.log(arr);
+  return arr;  // Return the arr elements
+}
+
+module.exports = { fn_sortArray } ;
