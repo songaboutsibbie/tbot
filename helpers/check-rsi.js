fd88fe4f4bfdb95f3fe125882606c6ddcd225530
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2022-12-16 16:53:50 +1100

    Add files via upload

--- helpers/check-rsi.js
+++ helpers/check-rsi.js
@@ -1,13 +1,16 @@
 // an asset price is convered overbought (due for a correction) when RSI is above 70 and oversold (due for a rebound) when it is below 30
 
-function fn_checkOverUnder(rsi_value) {
-  //checks whether a given crypto is overbought or underbought
-  if (rsi_value < 30 ) { 
-    // crypto is due for a rebound
-    return "oversold";
-  } else {
-    return "not oversold";
-  }
+function fn_isOversold(rsi_value) {
+  //checks whether a given crypto is oversold
+  if (rsi_value < 25 ) { return true; } 
+  else { return false; }
 }
 
-module.exports = { fn_checkOverUnder } ;
+function fn_isOversold_Recent(rsi_value_now, rsi_value_previous) {
+  //checks whether a given crypto has become oversold in the last interval
+  
+  if(rsi_value_now < 25 && rsi_value_previous > 25) { return true; } 
+  else { return false; }
+}
+
+module.exports = { fn_isOversold } ;
