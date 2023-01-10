ecfc494fc7df78e4da26319f021419309fedb364
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2023-01-10 19:53:29 +1100

    Add files via upload

--- helpers/check-rsi.js
+++ helpers/check-rsi.js
@@ -24,13 +24,15 @@ function fn_findLows(numbers) {
   var LocalLowArray = [];
   console.log("entered find lows")
   for (let i = 1; i < numbers.length; i++) {
-      console.log(Number(numbers[i]) + " < " + Number(numbers[i - 1]))
-      if (Number(numbers[i]) < Number(numbers[i - 1])) {
+     
+      console.log(numbers[i] + " < " + numbers[i - 1])
+
+      if (numbers[i] < numbers[i - 1]) {
         trend = "downward";
         console.log("trend downward")
       }
 
-      if (( Number(numbers[i]) > Number(numbers[i - 1]) ) && trend == "downward") {
+      if (numbers[i] > numbers[i - 1]  && trend == "downward") {
           trend = "upwards"
           LocalLowArray.push(numbers[i - 1]);
           console.log(numbers[i - 1] + " added to lower lows");
