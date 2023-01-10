48d5a9ba5cb11175e0a6393dc794066b7c354253
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2023-01-10 16:38:58 +1100

    Add files via upload

--- helpers/check-rsi.js
+++ helpers/check-rsi.js
@@ -13,4 +13,56 @@ function fn_isOversold_Recent(rsi_value_now, rsi_value_previous) {
   else { return false; }
 }
 
-module.exports = { fn_isOversold, fn_isOversold_Recent } ;
+function fn_detectTrend(data_array) {
+  if(data_array.value[0] < data_array.value[data_array.length]) { return "downward"; }
+  else { return "upward";}
+
+}
+
+function fn_findLows(number) {
+    // find lows
+    let trend = null;
+    var LocalLowArray = [];
+
+    for (let i = 1; i < number.length; i++) {
+
+        if (number[i] < number[i - 1]) {
+          trend = "downward";
+        }
+
+        if ((number[i] > number[i - 1]) && trend == "downward") {
+            trend = "upwards"
+            LocalLowArray.push(number[i - 1]);
+            console.log(number[i - 1] + " added to lower lows");
+        }
+    }
+    return LocalLowArray;
+}
+
+function fn_detectLowerLows(numbers) {
+    //  check if LowerLow is a downward trend or upward trend
+    if (numbers.length < 2) {
+        return null;
+    }
+
+    let uptrend = 0;
+    let downtrend = 0;
+
+
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > numbers[i - 1]) {
+            uptrend++;
+
+        } else if (numbers[i] < numbers[i - 1]) {
+            downtrend++
+        }
+  }
+
+  uptrend_percent = (uptrend / (uptrend + downtrend) ) * 100
+  downtrend_percent = (downtrend / (uptrend + downtrend) ) * 100
+  console.log("uptrend = " + uptrend_percent);
+  console.log("downtrend = " + downtrend_percent);
+
+}
+
+module.exports = { fn_isOversold, fn_isOversold_Recent, fn_detectTrend, fn_findLows, fn_detectLowerLows } ;
