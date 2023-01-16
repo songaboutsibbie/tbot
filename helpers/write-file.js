e73dbb62e935232ea1c6813c942390a27b8922a1
songaboutsibbie <106042547+songaboutsibbie@users.noreply.github.com>
2023-01-16 19:36:14 +1100

    Add files via upload

--- helpers/write-file.js
+++ helpers/write-file.js
@@ -8,4 +8,15 @@ function fn_writeBuyOrder(crypto) {
   });
 }
 
-module.exports = { fn_writeBuyOrder } ;  
+function fn_writeShortOrder(crypto) {
+  const fs = require('fs');
+  
+  console.log("writing to file");
+  message = crypto + ",";
+  fs.appendFile('./data/shortOrder.txt', message, function (err) {
+    if (err) throw err;
+  });
+}
+
+
+module.exports = { fn_writeBuyOrder, fn_writeShortOrder } ;  
